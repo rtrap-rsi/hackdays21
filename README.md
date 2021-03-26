d9987bf259267a51191847083cdf7363b65a1985
roberto.trapletti@rsi.ch <7623786Robby!>
2021-03-26 17:14:44 +0100

    Testing various algorithm

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Topic modeling - SRGSSR #hackdays21
 
 ### Context
+This repository contains the code used to do topics modeling on a specific dataset. It was developed in 2 days during the SRG SSR Hackdays 2021.
 
 ### Challenge
+A step of the challenge was to give users better experience giving tags on podcasts you will find on https://rsi.ch/podcast
+
 
 ### Proposed solution
 
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import gensim
 import gensim.corpora as corpora
 from pathlib import Path
 import logging as log
+import matplotlib.pyplot as plt
 
 # ----------------------------------------- CONFIG ----------------------------------------- #
 
@@ -26,27 +27,33 @@ if configs['logLevel'] != "None":
     log.basicConfig(level=configs['logLevel'])
 
 nlp = spacy.load(configs['spacy']['model'])
+
+
 # ----------------------------------------- FUNCTIONS ----------------------------------------- #
 
 # Add some custom stop words to the Spacy stop words list (words not needed for a semantic classification or clustering, like "the", "a", ecc)
 def prepareNLP():
-    stop_words = ["e","è","l'","i",'\n',"l’",'\n ','\n\n',"E","po'","“sono","d'","dell’","della","un"]
+    stop_words = ["e", "è", "l'", "i", '\n', "l’", '\n ', '\n\n', "E", "po'", "“sono", "d'", "dell’", "della", "un"]
     print("Updating Spacy stopwords list with customs: ")
     print(stop_words)
     for w in stop_words:
         nlp.vocab[w].is_stop = True
 
+
 # Clean text before the spacy pipeline, just do everything lowercase and little workaround for an error with the mallet implementation (.replace("”", ""))
-def get_clean_texts():
+def get_clean_texts(articles):
     print("Cleaning texts")
     return map(lambda x: x["transcription"].lower().replace("”", ""), articles)
+
 
 # Remove stop word and punctuation from a list of spacy Doc
 @Language.component('stopwords')
 def remove_stop_words(doc):
     # return token.lemma_ because we use these words in gensim
-    doc = [re.sub('[\W]+', '', token.lemma_.lower()) for token in doc if token.is_stop != True and token.is_punct != True]
+    doc = [re.sub('[\W]+', '', token.lemma_.lower()) for token in doc if
+           token.is_stop != True and token.is_punct != True]
     return doc
+
 
 # Return a list of Doc processed spacy object from a list of texts
 def process_text(docs):
@@ -58,22 +65,25 @@ def process_text(docs):
         doc_list.append(nlp(doc))
     return doc_list
 
+
 # Get articles from a folder, one article per file stored as json
 def getArticles():
     pprint(f'Getting articles from {configs["dataset"]}')
-    articles=[]
-    for file in glob.glob(f'{configs["dataset"]}/*',recursive=True):
+    articles = []
+    for file in glob.glob(f'{configs["dataset"]}/*', recursive=True):
         with open(file, "r", encoding="utf-8") as articlesJson:
             articles.append(json.load(articlesJson))
     return articles
+
 
 # This is a fake step, in our workflow we should take audios, transcribe and then tagging. For hackdays purpose we just trained our model on RSI articles
 def transcribe(articles):
     for article in articles:
         article["transcription"] = article["text"]
 
+
 # Test of the Latend Dirichlet Allocation statistical model
-def test_lda_model(corpus, words,processed_articles):
+def test_lda_model(corpus, words, processed_articles):
     print("\nRunning LDA model")
     if configs['topicModel']['loadFromFile'] == '':
         lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
@@ -88,53 +98,97 @@ def test_lda_model(corpus, words,processed_articles):
     else:
         lda_model = gensim.models.ldamodel.LdaModel.load(configs['topicModel']['loadFromFile'])
 
-    pprint(lda_model.print_topics(num_words=10))
-    pprint(lda_model.get_document_topics(corpus[0]))
+    pprint(lda_model.print_topics(num_words=20))
 
     print('Perplexity: ', lda_model.log_perplexity(corpus))  # a measure of how good the model is. lower the better.
-    coherence_model_lda = CoherenceModel(model=lda_model, texts=processed_articles, dictionary=words, coherence='c_v')
-    coherence_lda = coherence_model_lda.get_coherence()
-    print('Coherence Score: ', coherence_lda)
+    if configs['topicModel']['calculateCoherence']:
+        print('Calculating coherence...')
+        coherence_model_lda = CoherenceModel(model=lda_model, texts=processed_articles, dictionary=words,
+                                             coherence='c_v')
+        coherence_lda = coherence_model_lda.get_coherence()
+        print('Coherence Score: ', coherence_lda)
     return lda_model
 
+
 # Test of the Mallet LDA model
-def test_mallet_model(corpus, words,processed_articles):
+def test_mallet_model(corpus, words, processed_articles):
     print("\nRunning Mallet model")
     malletPath = "C:\\Users\\rober\\src\\mallet-2.0.8\\bin\\mallet.bat"
     if configs['topicModel']['loadFromFile'] == '':
-        ldamallet = gensim.models.wrappers.LdaMallet(malletPath, corpus=corpus, num_topics=configs['topicModel']['mallet']['num_topics'], id2word=words, alpha=configs['topicModel']['mallet']['alpha'])
+        ldamallet = gensim.models.wrappers.LdaMallet(malletPath, corpus=corpus,
+                                                     num_topics=configs['topicModel']['mallet']['num_topics'],
+                                                     id2word=words, alpha=configs['topicModel']['mallet']['alpha'],
+                                                     iterations=configs['topicModel']['mallet']['iterations'])
     else:
         ldamallet = gensim.models.wrappers.LdaMallet.load(configs['topicModel']['loadFromFile'])
-    pprint(ldamallet.show_topics(formatted=False))
-
-    coherence_model_mallet = CoherenceModel(model=ldamallet, texts=processed_articles, dictionary=words,
-                                         coherence='c_v')
-    coherence_mallet = coherence_model_mallet.get_coherence()
-    print('Coherence Score: ', coherence_mallet)
+    pprint(ldamallet.print_topics())
+    if configs['topicModel']['calculateCoherence']:
+        print('Calculating coherence...')
+        coherence_model_mallet = CoherenceModel(model=ldamallet, texts=processed_articles, dictionary=words,
+                                                coherence='c_v')
+        coherence_mallet = coherence_model_mallet.get_coherence()
+        print('Coherence Score: ', coherence_mallet)
     return ldamallet
 
+
 # Test of the HDP model
-def test_hdp_model(corpus, words,processed_articles):
+def test_hdp_model(corpus, words, processed_articles):
     print("\nRunning HDP model")
     if configs['topicModel']['loadFromFile'] == '':
         hdp_model = gensim.models.hdpmodel.HdpModel(corpus=corpus,
-                                                id2word=words)
+                                                    id2word=words)
     else:
         hdp_model = gensim.models.hdpmodel.HdpModel.load(configs['topicModel']['loadFromFile'])
 
     pprint(hdp_model.print_topics(num_words=10))
 
-    coherence_model_lda = CoherenceModel(model=hdp_model, texts=processed_articles, dictionary=words, coherence='c_v')
-    coherence_lda = coherence_model_lda.get_coherence()
-    print('Coherence Score: ', coherence_lda)
+    if configs['topicModel']['calculateCoherence']:
+        print('Calculating coherence...')
+        coherence_model_lda = CoherenceModel(model=hdp_model, texts=processed_articles, dictionary=words,
+                                             coherence='c_v')
+        coherence_lda = coherence_model_lda.get_coherence()
+        print('Coherence Score: ', coherence_lda)
     return hdp_model
+
 
 def load_processed():
     return np.load("processedArticles.npy", allow_pickle=True)
 
+
 def save_processed(processed):
     print("Saving processed articles")
-    np.save("processedArticles.npy",processed)
+    np.save("processedArticles.npy", processed)
+
+
+def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
+    """
+    Compute c_v coherence for various number of topics
+
+    Parameters:
+    ----------
+    dictionary : Gensim dictionary
+    corpus : Gensim corpus
+    texts : List of input texts
+    limit : Max num of topics
+
+    Returns:
+    -------
+    model_list : List of LDA topic models
+    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
+    """
+    malletPath = "C:\\Users\\rober\\src\\mallet-2.0.8\\bin\\mallet.bat"
+    coherence_values = []
+    model_list = []
+    for num_topics in range(start, limit, step):
+        model = gensim.models.wrappers.LdaMallet(malletPath, corpus=corpus, num_topics=num_topics, id2word=dictionary,
+                                                 alpha=configs['topicModel']['mallet']['alpha'])
+        model_list.append(model)
+
+        coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
+        coherence_values.append(coherencemodel.get_coherence())
+
+    return model_list, coherence_values
+
 
 # ----------------------------------------- MAIN ----------------------------------------- #
 
@@ -151,7 +205,7 @@ if __name__ == '__main__':
     else:
         articles = getArticles()
         transcribe(articles)
-        texts = get_clean_texts()
+        texts = get_clean_texts(articles)
         processed_articles = process_text(texts)
         save_processed(processed_articles)
 
@@ -173,6 +227,26 @@ if __name__ == '__main__':
         model = test_hdp_model(corpus, words, processed_articles)
     else:
         print(f"No model named {modelName} found")
+
+    if configs['testDocument']:
+        test_articles = []
+        with open(Path(configs['testDocument']), "r", encoding="utf-8") as articleJson:
+            test_articles.append(json.load(articleJson))
+        transcribe(test_articles)
+        test_texts = get_clean_texts(test_articles)
+        test_processed_articles = process_text(test_texts)
+        test_words = corpora.Dictionary(test_processed_articles)
+        test_corpus = [words.doc2bow(doc) for doc in test_processed_articles]
+        print(model[test_corpus[0]])
+        doc_topics = model[test_corpus[0]]
+        most_relevant_doc_topic_id = max(doc_topics,key=lambda i:i[1])[0]
+        # print(most_relevant_doc_topic_id)
+        print(model.show_topic(most_relevant_doc_topic_id, topn=20))
+
+        # print("Search best num topics")
+        # model_list, coherence_values = compute_coherence_values(dictionary=words, corpus=corpus,
+        #                                                         texts=processed_articles, start=2, limit=40, step=6)
+        # print(coherence_values)
 
     if configs['topicModel']['saveAs'] and model != None and configs['topicModel']['loadFromFile'] == '':
         print('Saving topic model...')
